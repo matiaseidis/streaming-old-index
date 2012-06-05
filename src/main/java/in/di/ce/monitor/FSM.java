2fd9123b9a74f2b2ce6feac7f6d35d1262860a62
Matias Eidis <matiaseidis@gmail.com>
2012-06-05 12:05:09 -0300

    bla2

--- src/main/java/in/di/ce/monitor/FSM.java
+++ src/main/java/in/di/ce/monitor/FSM.java
@@ -8,21 +8,14 @@ import org.apache.log4j.Logger;
 
 public class FSM {
 
-	private Notifier notifier = new Notifier();
+	private Notifier notifier;
 	public Logger logger = Logger.getLogger(getClass());
 
 	private static final String MONITORED_DIR = "/home/meidis/testFolder";
 	private static final long INTERVAL = 3000;
 
-	public static void main(String[] args) {
-		try {
-			new FSM().begin();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
 	public void begin() throws Exception {
+		notifier = new Notifier();
 		File directory = new File(MONITORED_DIR);
 		FileAlterationObserver observer = new FileAlterationObserver(directory);
 		observer.addListener(new SharedVideosListener(notifier));
@@ -31,4 +24,13 @@ public class FSM {
 		monitor.addObserver(observer);
 		monitor.start();
 	}
+
+	public static void main(String[] args) {
+		try {
+			new FSM().begin();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
+
