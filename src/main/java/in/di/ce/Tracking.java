cb25bdee1e8f47a4f0b24c03eca9e9945c6cd13a
Matias Eidis <matiaseidis@gmail.com>
2012-06-19 09:54:39 -0300

    tamaño del primer cacho del plan limitado a un max de 32 mb

--- src/main/java/in/di/ce/Tracking.java
+++ src/main/java/in/di/ce/Tracking.java
@@ -14,50 +14,51 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class Tracking implements Serializable {
-	
+
 	private static final Log log = LogFactory.getLog(Tracking.class);
 
+	private static final int MAX_FIRST_CACHO_SIZE = 32;
 	private static final int MAX_CACHO_SIZE = 64; 
 
 	/*
 	 * videos repository
 	 */
 	private final ConcurrentMap<String, Video> videos = new ConcurrentHashMap<String, Video>();
-	
+
 	/*
 	 * chunks repository
 	 * [videoId:[userId:[chunks]]]
 	 */
 	private final ConcurrentMap<String, Map<String, List<Integer>>> usersLocalRepoTracking = new ConcurrentHashMap<String, Map<String,List<Integer>>>();
-	
+
 	public boolean registerVideo(Video video){
 		return videos.putIfAbsent(video.getId(), video) == null;
 	}
-	
+
 	public boolean registerChunks(String videoId, String userId, List<Integer> chunks){
-		
+
 		if(usersLocalRepoTracking.get(videoId) == null){
 			throw new IllegalStateException("Can't register chunks for video: "+videoId+". there is no such video in database.");
 		}
-		
+
 		if (usersLocalRepoTracking.get(videoId).get(userId) == null){
 			usersLocalRepoTracking.get(videoId).put(userId, new ArrayList<Integer>());
 			log.info("adding chunk list for video "+ videoId + " for user " + userId);
 		}
-		
+
 		return usersLocalRepoTracking.get(videoId).get(userId).addAll(chunks);
 	}
-	
+
 	public boolean unregisterChunks(String videoId, String userId, List<Integer> chunks){
-		
+
 		if (!usersLocalRepoTracking.containsKey(videoId) || !usersLocalRepoTracking.get(videoId).containsKey(userId)){ 
 			return false;
 		}
 		return usersLocalRepoTracking.get(videoId).get(userId).removeAll(chunks);
 	}
-	
+
 	public List<Integer>getChunksFrom(String videoId, String userId){
-		
+
 		if (!usersLocalRepoTracking.containsKey(videoId) || !usersLocalRepoTracking.get(videoId).containsKey(userId)){ 
 			return null;
 		}
@@ -73,22 +74,22 @@ public class Tracking implements Serializable {
 	}
 
 	public List<UserChunks> grafo(String videoId, String userId) {
-		
+
 		Map<String, List<Integer>> usersWithChunks = usersLocalRepoTracking.get(videoId);
-		
+
 		if(MapUtils.isEmpty(usersWithChunks)){
 			log.info("Unable to ellaborate retrieving plan for video "+videoId+" for user "+userId);
 			return null;
 		}
-		
+
 		UserChunks thisUserChunks = new UserChunks(userId, new ArrayList<Integer>());
-		
+
 		/*
 		 * userId:[chunkOrdinals]
 		 */
-//		Map<String, List<Integer>> chunks = new HashMap<String, List<Integer>>();
+		//		Map<String, List<Integer>> chunks = new HashMap<String, List<Integer>>();
 		Map<String, UserChunks> userChunks = new HashMap<String, UserChunks>();
-		
+
 		if(usersWithChunks.containsKey(userId) && CollectionUtils.isNotEmpty(usersWithChunks.get(userId))){
 			/*
 			 * el usuario tiene chunks de este video
@@ -96,11 +97,11 @@ public class Tracking implements Serializable {
 			thisUserChunks.getChunks().addAll(usersWithChunks.get(userId));
 			userChunks.put(userId, thisUserChunks);
 		}
-		
+
 		Video video = this.getVideo(videoId);
-		
+
 		List<UserChunks> userChunkList = new ArrayList<UserChunks>();
-		
+
 		for(int i = 0; i<video.getChunks().size(); i++) {
 			if(thisUserChunks.getChunks().contains(i)) {
 				UserChunks uc = segmentFrom(i, thisUserChunks);
@@ -112,7 +113,7 @@ public class Tracking implements Serializable {
 				i+=poorUserChunks.getChunks().size();
 			}
 		}
-		
+
 		int total = 0;
 		for(UserChunks uc : userChunkList){
 			total+=uc.getChunks().size();
@@ -124,57 +125,59 @@ public class Tracking implements Serializable {
 		return userChunkList;
 
 	}
-private UserChunks getShorterUserChunksFrom(int i, Map<String, List<Integer>> usersWithChunks) {
-	
-	//[userId: to]
-	Map<String, Integer> temp = new HashMap<String, Integer>();
-	int minor = -1;
-	for(Map.Entry<String, List<Integer>> entry: usersWithChunks.entrySet()) {
-		if(entry.getValue().contains(i)){
-			int current = i++;
-			while(true) {
-				if(!entry.getValue().contains(current) || current == MAX_CACHO_SIZE) {
-					break;
+	private UserChunks getShorterUserChunksFrom(int i, Map<String, List<Integer>> usersWithChunks) {
+
+		int max = i == 0 ? MAX_FIRST_CACHO_SIZE : MAX_CACHO_SIZE;
+
+		//[userId: to]
+		Map<String, Integer> temp = new HashMap<String, Integer>();
+		int minor = -1;
+		for(Map.Entry<String, List<Integer>> entry: usersWithChunks.entrySet()) {
+			if(entry.getValue().contains(i)){
+				int current = i++;
+				while(true) {
+					if(!entry.getValue().contains(current) || current == max) {
+						break;
+					}
+					current++;
 				}
-				current++;
+				temp.put(entry.getKey(), current);
+				if(minor == -1) {
+					minor = current;
+				} 
+				if(minor > current) {
+					minor = current;
+				} 
 			}
-			temp.put(entry.getKey(), current);
-			if(minor == -1) {
-				minor = current;
-			} 
-			if(minor > current) {
-				minor = current;
-			} 
 		}
+
+		for(Map.Entry<String, Integer> entry : temp.entrySet()){
+			if(entry.getValue() == minor){
+				UserChunks uc = new UserChunks(entry.getKey(), new ArrayList<Integer>());
+				for(int ordinal = 0; ordinal < minor; ordinal++) {
+					uc.getChunks().add(ordinal);
+				}
+				return uc;
+			}
+		}
+		return null;
 	}
 
-	for(Map.Entry<String, Integer> entry : temp.entrySet()){
-		if(entry.getValue() == minor){
-			UserChunks uc = new UserChunks(entry.getKey(), new ArrayList<Integer>());
-			for(int ordinal = 0; ordinal < minor; ordinal++) {
-				uc.getChunks().add(ordinal);
+	private UserChunks segmentFrom(int from, UserChunks thisUserChunks) {
+		UserChunks result = new UserChunks(thisUserChunks.getUserId(), new ArrayList());
+		result.getChunks().add(from);
+		int current = from++;
+		while(true) {
+			if(!thisUserChunks.getChunks().contains(current)) {
+				break;
 			}
-			return uc;
+			result.getChunks().add(current);
 		}
+		return result;
 	}
-	return null;
-	}
-
-private UserChunks segmentFrom(int from, UserChunks thisUserChunks) {
-	UserChunks result = new UserChunks(thisUserChunks.getUserId(), new ArrayList());
-	result.getChunks().add(from);
-	int current = from++;
-	while(true) {
-		if(!thisUserChunks.getChunks().contains(current)) {
-			break;
-		}
-		result.getChunks().add(current);
-	}
-	return result;
-}
 
 	public String getVideoIdFromFilename(String fileName) {
-		
+
 		for(Map.Entry<String, Video> entry : videos.entrySet()){
 			if(entry.getValue().getFileName().equals(fileName)){
 				return entry.getKey();
@@ -182,6 +185,6 @@ private UserChunks segmentFrom(int from, UserChunks thisUserChunks) {
 		}
 		return null;
 	}
-	
-	
+
+
 }
